cd927a2639f9b8ae727e6f0cf6591485e82fa407
Ahmed Amin Hassan <ahmedAminHassanIsmail@gmail.com>
2022-03-22 22:15:39 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Ahmed Amin on 22/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
